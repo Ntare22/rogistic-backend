ffa70edf5125c24b685f9dc3994e22391221d0c0
Samuel N NIYITANGA <sniyitanga@bk.rw>
2021-06-23 20:50:39 +0200

    added database configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { HomeModule } from './home/home.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: true
+      autoSchemaFile: true,
     }),
-    HomeModule
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DDB_DATABASE'),
+      }),
+    }),
+    HomeModule,
   ],
   controllers: [],
   providers: [],
